4ee69a1c2002237dfee96ca755cb0578de649636
Bob Baker <bbaker@sharecare.com>
2016-11-10 08:42:44 -0500

    Stripped off the leading / character in the strings that come back from the selector

--- slideshows-app/src/main/java/com/sharecare/cms/featured/remote/RemoteFeaturedSlideshowsRequestBuilder.java
+++ slideshows-app/src/main/java/com/sharecare/cms/featured/remote/RemoteFeaturedSlideshowsRequestBuilder.java
@@ -3,14 +3,17 @@ package com.sharecare.cms.featured.remote;
 import com.google.inject.Inject;
 import com.sharecare.cms.featured.schema.FeaturedJCRSchema;
 import com.sharecare.sdk.slideshows.featured.model.FeaturedSlideshowRequest;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
-import static com.sharecare.cms.utils.NodeUtils.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.sharecare.cms.utils.NodeUtils.extractMultiField;
 import static com.sharecare.cms.utils.NodeUtils.fromNode;
 
+@Slf4j
 public class RemoteFeaturedSlideshowsRequestBuilder implements FeaturedSlideshowsRequestBuilder {
 
     @Inject
@@ -20,10 +23,25 @@ public class RemoteFeaturedSlideshowsRequestBuilder implements FeaturedSlideshow
     @Override
     public FeaturedSlideshowRequest forNode(Node node) throws RepositoryException {
 
+        // Before we send off the request to the API service, we have some
+        // housecleaning to do.  Because the data coming from the selector
+        // starts with a `/` character, we need to strip that off.
+        String hero = fromNode(FeaturedJCRSchema.hero.name(), node);
+        if (hero.startsWith("/"))
+            hero = hero.substring(1);
+
+        List<String> uriList = extractMultiField(node, FeaturedJCRSchema.carousel.name());
+        List<String> carousel = uriList.stream()
+                .map(slideshow -> slideshow.startsWith("/")
+                        ? slideshow.substring(1)
+                        : slideshow).collect(Collectors.toList());
+
         FeaturedSlideshowRequest request = FeaturedSlideshowRequest.builder()
-                .hero(fromNode(FeaturedJCRSchema.hero.name(), node))
-                .carousel(extractMultiField(node, FeaturedJCRSchema.carousel.name()))
+                .hero(hero)
+                .carousel(carousel)
                 .build();
+
+        log.info("Created Featured request object {} ", request);
 
         return request;
     }
