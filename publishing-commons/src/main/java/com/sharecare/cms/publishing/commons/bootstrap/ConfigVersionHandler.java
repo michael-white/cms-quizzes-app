38a2993685cb6570250a61e7fd8155f242b27de0
Dian Andonov <dandonov@sharecare.com>
2016-11-29 17:00:07 -0500

    Temp adding bootstrap tasks to clean up the ssl filter left in the JCR from some testing

--- publishing-commons/src/main/java/com/sharecare/cms/publishing/commons/bootstrap/ConfigVersionHandler.java
+++ publishing-commons/src/main/java/com/sharecare/cms/publishing/commons/bootstrap/ConfigVersionHandler.java
@@ -1,70 +1,89 @@
 package com.sharecare.cms.publishing.commons.bootstrap;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.NodeExistsDelegateTask;
 import info.magnolia.module.delta.RemoveNodeTask;
 import info.magnolia.module.delta.Task;
+import info.magnolia.module.model.Version;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConfigVersionHandler extends GlobalVersionHandler {
 
-	private static final String OWNER = "dan@sharecare.com";
+    private static final String OWNER = "dan@sharecare.com";
 
-	private static final String LICENSE = "LS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tClZlcnNpb246IEJDUEcgdjEuNDYKCm93SjRuSnZBeTh6RXhMaDQ4eU9HcFluL3RSbFB6MDdpemszUHk4bkpURTdOSzA0TisxdXhWRGtrbzFUQk43RkkKd2RoUXdkRFV5c2pReXNCU3dkVWxSTUhJd05DTXF5eTFxRGd6UDYvWU50cFV6MERQUU1jTVJFWno1WmZucFJiWgpwaVRtT1JSbkpCYWxKZ094WG5KK0xsZHFSVUZtVVdJSlVJY3RVTHU1cm9HeHJwRUJWMjUrU21sT2FyRnRhbDVKCmFsRkJVV1p4S2xkcVNtWUoyRndrc1k0NUxFeU1UQXhzckV3Z2R6RndjUXJBM08xNGlQbS82K2VXN3FveXhYTjkKSFhteXpKdGt4TDJpM3pOejZzOVdWcHpFOGI1TzczSnNvN3pKWXViVEYvajlwODErOU9yelViKzR5V3FUSjd1KwpmYVBETzMvWFRua3JMNlhhcFh0ckZodzMzbEt3VGUzOWZ5Rm5WNUZUSlcrRDl6WWUxMXZNcGRqZ0dYMjZ0K2w1Cit3bi9NbzdOTDcwL3ZkbHp1ZFJuOXdxanE1SXpWcWFGM1RiZTR4Z0RBSmhoZ2ZzPQo9RXRQagotLS0tLUVORCBQR1AgTUVTU0FHRS0tLS0tCg==";
+    private static final String LICENSE = "LS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tClZlcnNpb246IEJDUEcgdjEuNDYKCm93SjRuSnZBeTh6RXhMaDQ4eU9HcFluL3RSbFB6MDdpemszUHk4bkpURTdOSzA0TisxdXhWRGtrbzFUQk43RkkKd2RoUXdkRFV5c2pReXNCU3dkVWxSTUhJd05DTXF5eTFxRGd6UDYvWU50cFV6MERQUU1jTVJFWno1WmZucFJiWgpwaVRtT1JSbkpCYWxKZ094WG5KK0xsZHFSVUZtVVdJSlVJY3RVTHU1cm9HeHJwRUJWMjUrU21sT2FyRnRhbDVKCmFsRkJVV1p4S2xkcVNtWUoyRndrc1k0NUxFeU1UQXhzckV3Z2R6RndjUXJBM08xNGlQbS82K2VXN3FveXhYTjkKSFhteXpKdGt4TDJpM3pOejZzOVdWcHpFOGI1TzczSnNvN3pKWXViVEYvajlwODErOU9yelViKzR5V3FUSjd1KwpmYVBETzMvWFRua3JMNlhhcFh0ckZodzMzbEt3VGUzOWZ5Rm5WNUZUSlcrRDl6WWUxMXZNcGRqZ0dYMjZ0K2w1Cit3bi9NbzdOTDcwL3ZkbHp1ZFJuOXdxanE1SXpWcWFGM1RiZTR4Z0RBSmhoZ2ZzPQo9RXRQagotLS0tLUVORCBQR1AgTUVTU0FHRS0tLS0tCg==";
 
-	/**
-	 * Forces extraction of the module's files, like template JSPs, from
-	 * <code>classes/mgnl-files</code> into the web application's root. This
-	 * does not yield a "Magnolia needs to be updated" screen. To avoid
-	 * destroying changes, Magnolia will not override files which have been
-	 * modified in the web application.
-	 * (In Magnolia, see also Tools, Development tools, to reload at request.)
-	 */
-	@Override
-	protected List<Task> getStartupTasks(InstallContext installContext) {
-		log.info("Running ConfigVersionHandler.getStartupTasks()");
-		List<Task> tasks = super.getStartupTasks(installContext);
+    /**
+     * Forces extraction of the module's files, like template JSPs, from
+     * <code>classes/mgnl-files</code> into the web application's root. This
+     * does not yield a "Magnolia needs to be updated" screen. To avoid
+     * destroying changes, Magnolia will not override files which have been
+     * modified in the web application.
+     * (In Magnolia, see also Tools, Development tools, to reload at request.)
+     */
+    @Override
+    protected List<Task> getStartupTasks(InstallContext installContext) {
+        log.info("Running ConfigVersionHandler.getStartupTasks()");
+        List<Task> tasks = super.getStartupTasks(installContext);
 
-		log.info("Running ConfigVersionHandler - updating license key.");
+        log.info("Running ConfigVersionHandler - updating license key.");
 
-		tasks.add(new OverridePropertyValueTask(
-				"License",
-				"Sets up enterprise",
-				"config",
-				"/modules/enterprise/license",
-				"key",
-				"",
-				LICENSE));
+        tasks.add(new OverridePropertyValueTask(
+                "License",
+                "Sets up enterprise",
+                "config",
+                "/modules/enterprise/license",
+                "key",
+                "",
+                LICENSE));
 
-		tasks.add(new OverridePropertyValueTask(
-				"License",
-				"Sets up enterprise",
-				"config",
-				"/modules/enterprise/license",
-				"owner",
-				"",
-				OWNER));
+        tasks.add(new OverridePropertyValueTask(
+                "License",
+                "Sets up enterprise",
+                "config",
+                "/modules/enterprise/license",
+                "owner",
+                "",
+                OWNER));
 
-		tasks.add(new NodeExistsDelegateTask("ExistingPubInstances",
-				"Checking for existing pub subscribers",
-				"config",
-				"/server/activation/subscribers",
-				new RemoveNodeTask("Removing existing pub subscribers",
-						"Activation does not need pub instances",
-						"config",
-						"/server/activation/subscribers")));
+        tasks.add(new NodeExistsDelegateTask("ExistingPubInstances",
+                "Checking for existing pub subscribers",
+                "config",
+                "/server/activation/subscribers",
+                new RemoveNodeTask("Removing existing pub subscribers",
+                        "Activation does not need pub instances",
+                        "config",
+                        "/server/activation/subscribers")));
 
-		return tasks;
-	}
+        return tasks;
+    }
 
-	@Override
-	protected List<Task> getExtraInstallTasks(InstallContext ctx) {
+    @Override
+    protected List<Task> getDefaultUpdateTasks(Version forVersion) {
+        List<Task> tasks = super.getDefaultUpdateTasks(forVersion);
 
-		log.info("Running ConfigVersionHandler.getExtraInstallTasks()");
-		return super.getExtraInstallTasks(ctx);
-	}
+        tasks.add(removeSSLFilter);
+        return tasks;
+    }
+
+
+    @Override
+    protected List<Task> getExtraInstallTasks(InstallContext ctx) {
+
+        log.info("Running ConfigVersionHandler.getExtraInstallTasks()");
+        return super.getExtraInstallTasks(ctx);
+    }
+
+
+    private static NodeExistsDelegateTask removeSSLFilter = new NodeExistsDelegateTask("Check if SSL Filter is registered",
+            "checking for the existence of /server/filters/ssl",
+            "config",
+            "/server/filters/ssl",
+            new RemoveNodeTask("Remove the Secure redirect filter",
+                    "Removing the node /server/filters/ssl",
+                    "config",
+                    "/server/filters/ssk"));
 }
